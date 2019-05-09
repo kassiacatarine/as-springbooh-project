49ae39b77c4b0782374cae9848b8f6a274dde9ed
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-05-09 20:20:36 -0300

    Adicionando estrutura de projeto inicial com o spring boot

---
+++ src/main/java/br/edu/utfpr/asspringbootproject/AsSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.asspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsSpringbootProjectApplication.class, args);
+	}
+
+}
