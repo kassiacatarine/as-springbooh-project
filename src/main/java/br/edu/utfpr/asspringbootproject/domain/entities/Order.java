0648e1783a46e299851c9f9b8e6becc1906f71c1
Kássia Catarine <kassia_catarine.15@hotmail.com>
2019-05-09 21:17:10 -0300

    Classe Order

--- src/main/java/br/edu/utfpr/asspringbootproject/domain/entities/Order.java
+++ src/main/java/br/edu/utfpr/asspringbootproject/domain/entities/Order.java
@@ -1,8 +1,31 @@
 package br.edu.utfpr.asspringbootproject.domain.entities;
 
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Order
  */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Order {
-
+    @Id
+    private Long id;
+    private CreditCard creditCard;
+    private Date orderDate;
+    private String creditCardNumber;
+    private List<OrderLine> orderLines;
+    private Float total;
+    private Address deliveryAddress;
+    private String creditCardExpiryDate;
 }
